49c2198d899ba1c94a86766ef7a26cf9798639d2
MatZenSn <Matiasvinias@gmail.com>
2021-08-17 19:40:12 -0300

    Campos Obligatorios en el login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e){ 
 
+      var  mail = document.getElementById("mail")
+       var password = document.getElementById("password")
+    document.getElementById("butonLogin").addEventListener("click",function(){
+        if(mail.value != "" || password.value != ""){
+        location.replace("inicio.html")}
+    })
+
+    
 });
