7ad12f534eb10529462e66cc383989b7664a1ba4
MatZenSn <Matiasvinias@gmail.com>
2021-08-20 18:27:16 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(e){
 
     document.getElementById("buttonLogin").addEventListener("click",function(){
         
-      var  user = document.getElementById("mail").value
+      var  user = document.getElementById("user").value
       var password = document.getElementById("password").value
       
         if(user != "" && password != ""){ //Si los campos son diferentes de nada ejecuta location.replace, que redirije sin permitir volver atras con el boton de back del navegador.
