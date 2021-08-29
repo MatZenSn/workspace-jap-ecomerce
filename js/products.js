f085954303ec1280d6a801ef5c3f4b54ae74495e
MatZenSn <Matiasvinias@gmail.com>
2021-08-29 10:30:22 -0300

    Filtros de ordenamiento Products

--- js/products.js
+++ js/products.js
@@ -1,50 +1,134 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-
+const ORDER_ASC_BY_COST = "AZ";
+const ORDER_DESC_BY_COST = "ZA";
+var currentSortCriteria = undefined;
+const ORDER_BY_PROD_COUNT = "Cant.";
+var minCost = undefined;
+var maxCost = undefined;
 var currentProductsArray = [];
 
-document.addEventListener("DOMContentLoaded", function (e){
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_COST)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_COST){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_COUNT){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
 
-    
-    //creo funcion para mostrar los producos
-    function showProductsList(){ 
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
 
+    return result;
+}
 
-        for(let i = 0; i < currentProductsArray.length; i++){ // Utilizo un for para recorrer el array recibido
-            let product = currentProductsArray[i];
-            //Lo siguiente agrega la infrmacion al div con la id "product-list-container" con sus clases de bootstrap correspondientes
-            document.getElementById("product-list-container").innerHTML += ` 
-                <a href="product-info.html" class="list-group-item list-group-item-action">
-                    <div class="row">
-                        <div class="col-3">
-                            <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                        </div>
-                        <div class="col">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1">`+ product.name +`</h4>
-                                <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
-                            </div>
-                            <p class="mb-1">` + product.description + `</p> <br
-                        
-                            
-                            <div> <b>` +  product.currency + ` ` + product.cost + `</b> </div>
-                            
-                        </div>
+function showProductsList(){ //creo funcion para mostrar los producos
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){ // Utilizo un for para recorrer el array recibido
+        let product = currentProductsArray[i];
+
+        if (((minCost == undefined) || (minCost != undefined && parseInt(product.cost) >= minCost)) &&
+            ((maxCost == undefined) || (maxCost != undefined && parseInt(product.cost) <= maxCost))){
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                     </div>
-                </a>
-                `
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p> <br
+                        <div> <b>` +  product.currency + ` ` + product.cost + `</b> </div>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
+
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    };
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    //Muestro los productos ordenadas
+    showProductsList();
+};
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){ // Utilizo getJSONData creada en init.js
+        if (resultObj.status === "ok"){
+            sortAndShowProducts(ORDER_ASC_BY_COST, resultObj.data);
+        } 
+    });
+
+        document.getElementById("sortAsc").addEventListener("click", function(){
+            sortAndShowProducts(ORDER_ASC_BY_COST);
+        });
+
+        document.getElementById("sortDesc").addEventListener("click", function(){
+            sortAndShowProducts(ORDER_DESC_BY_COST);
+        });
+        document.getElementById("sortByCount").addEventListener("click", function(){
+            sortAndShowProducts(ORDER_BY_PROD_COUNT);
+        });
+        document.getElementById("clearRangeFilter").addEventListener("click", function(){
+            document.getElementById("rangeFilterCostMin").value = "";
+            document.getElementById("rangeFilterCostMax").value = "";
+
+            minCost = undefined;
+            maxCost = undefined;
+
+            showProductsList();
+        });
+
+        document.getElementById("rangeFilterCost").addEventListener("click", function(){
+            //Obtengo el mínimo y máximo de los intervalos para filtrar por precio
+        
+            minCost = document.getElementById("rangeFilterCostMin").value;
+            maxCost = document.getElementById("rangeFilterCostMax").value;
+    
+            if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0){
+                minCost = parseInt(minCost);
+            }
+            else{
+                minCost = undefined;
             }
     
-            
-            
-        };
-
-
-        getJSONData("https://japdevdep.github.io/ecommerce-api/product/all.json").then(function(result){ // Utilizo getJSONData creada en init.js
-            if (result.status === "ok"){
-                currentProductsArray = result.data
-                showProductsList();} //Llamo la funcion creada mas arriba
-            })
-
-})
+            if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
+                maxCost = parseInt(maxCost);
+            }
+            else{
+                maxCost = undefined;
+            }
+    
+            showProductsList();
+        });
+});
